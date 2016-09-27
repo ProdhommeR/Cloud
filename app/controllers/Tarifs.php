8fd16040660e4082cf479e622e1b2b20c811b8e5
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-09-27 10:49:43 +0200

    First commit

---
+++ app/controllers/Tarifs.php
@@ -0,0 +1,20 @@
+<?php
+class Tarifs extends \_DefaultController {
+	public function isValid(){
+		return Auth::isAuth();
+	}
+	public function onInvalidControl(){
+		$this->messageDanger("Vous n'êtes pas autoriser à afficher cette page !",3000,false);
+		exit();
+	}
+	public function __construct() {
+		// TODO Auto-generated method stub
+		parent::__construct ();
+		$this->model = "Tarif";
+		$this->title = "Tarif";
+	}
+	public function frm($id = NULL) {
+		$tarif = $this->getInstance ( $id );
+		$this->loadView("Tarif/edit.html",array("tarif"=>$tarif));
+	}
+}
