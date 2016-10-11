44f311be75f43b3b0ba0341af77eda0802b64919
AnthoLuq <anthony.luquas@sts-sio-caen.info>
2016-10-11 10:41:39 +0200

    Liste Disque
    Test en commentaire liste disque

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -11,11 +11,30 @@ class MyDisques extends Controller{
 	public function index() {
 		echo Jquery::compile();
 	}
-
+	
 	public function finalize(){
 		if(!RequestUtils::isAjax()){
 			$this->loadView("main/vFooter.html");
 		}
 	}
+	
+	public function __construct() {
+		parent::__construct ();
+		$this->title = "Disques";
+		$this->model = "Disque";
+	}
 
 }
+
+/*public function Users($search = NULL) {
+		$where = "1=1";
+		if (isset ( $search )) {
+			$where = "login like '%($search)%' or mail like '%($search)%'";
+		}
+		$users = DAO::getAll ( "Utilisateur", $where );
+		$this->loadView ( "First/users.html", array (
+				"users" => $users 
+		) );
+		echo Jquery::getOn ( "click", ".c_user", "First/showUser", "#divUser" );
+	}
+	*/
