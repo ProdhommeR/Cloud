bb1fb8ae0cfb54e76c5a1dfe7fe3ec3e8f9fc3fe
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-10-13 09:38:22 +0200

    Commit Anthony

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -2,39 +2,39 @@
 use micro\controllers\Controller;
 use micro\js\Jquery;
 use micro\utils\RequestUtils;
-class MyDisques extends Controller{
-	public function initialize(){
-		if(!RequestUtils::isAjax()){
-			$this->loadView("main/vHeader.html",array("infoUser"=>Auth::getInfoUser()));
+class MyDisques extends Controller {
+	public function initialize() {
+		if (! RequestUtils::isAjax ()) {
+			$this->loadView ( "main/vHeader.html", array (
+					"infoUser" => Auth::getInfoUser () 
+			) );
 		}
 	}
 	public function index() {
-		echo Jquery::compile();
+		echo Jquery::compile ();
 	}
-	
-	public function finalize(){
-		if(!RequestUtils::isAjax()){
-			$this->loadView("main/vFooter.html");
+	public function finalize() {
+		if (! RequestUtils::isAjax ()) {
+			$this->loadView ( "main/vFooter.html" );
 		}
 	}
-	
 	public function __construct() {
 		parent::__construct ();
 		$this->title = "Disques";
 		$this->model = "Disque";
 	}
-
-}
-
-/*public function Users($search = NULL) {
+	public function Disques($search = NULL) {
 		$where = "1=1";
 		if (isset ( $search )) {
-			$where = "login like '%($search)%' or mail like '%($search)%'";
+			$where = "id like '%($search)%' or nom like '%($search)%'";
 		}
-		$users = DAO::getAll ( "Utilisateur", $where );
-		$this->loadView ( "First/users.html", array (
-				"users" => $users 
+		$users = DAO::getAll ( "Disque", $where );
+		$this->loadView ( "First/disque.html", array (
+				"disques" => $disque 
 		) );
-		echo Jquery::getOn ( "click", ".c_user", "First/showUser", "#divUser" );
+		echo Jquery::getOn ( "click", ".c_disque", "First/showdisque", "#divDisque" );
 	}
-	*/
+}
+
+	
+	
