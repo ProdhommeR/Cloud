c8518d23104d10a6c7ba2cb2cd7d25aea9be65d0
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-10-17 09:03:24 +0200

    Lister mes disques

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -27,40 +27,31 @@ class MyDisques extends Controller {
 			$this->loadView ( "main/vFooter.html" );
 		}
 	}
-
 	public function __construct() {
 		parent::__construct ();
 		$this->title = "Disques";
 		$this->model = "Disque";
 	}
 	public function Disques($search = NULL) {
-		
-		$where = "idutilisateur=".Auth::getUser()->getId();
-		if(isset($search)){
-			$where.=" and nom like '%".$search."%'";
+		$where = "idutilisateur=" . Auth::getUser ()->getId ();
+		if (isset ( $search )) {
+			$where .= " and nom like '%" . $search . "%'";
 		}
 		$disques = DAO::getAll ( "Disque", $where );
 		$this->loadView ( "disques/MesDisques.html", array (
 				"disques" => $disques 
 		) );
-		//echo Jquery::getOn ( "click", ".c_disque", "First/showUser", "#divUser" );
+		 echo Jquery::getOn ( "click", ".c_disque", "MyDisques/showDisk", "#divDisk" );
+	}
+	public function showDisk($id) {
+		$disque = DAO::getOne ( "Disque", $id );
+		if (is_null ( $disque ) === false) {
+			echo "Nom du disque : " . $disque->getnom () . "<br>";
+			echo "Mémoire allouée : " . DirectoryUtils::formatBytes($disque->getQuota ());
+		} else {
 			
-	
-	
-
-}
-
-/*public function Disques($search = NULL) {
-		$where = "1=1";
-		if (isset ( $search )) {
-			$where = "id like '%($search)%' or nom like '%($search)%'";
 		}
-		$users = DAO::getAll ( "Disque", $where );
-		$this->loadView ( "First/disque.html", array (
-				"disques" => $disque 
-		) );
-		echo Jquery::getOn ( "click", ".c_disque", "First/showdisque", "#divDisque" );
-	}*/
+	}
 }
 
 	
