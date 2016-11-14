ba877120a95a244b9c1fa6411d277abba992807f
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-11-14 11:32:33 +0100

    commit Travis/Selenium 2.0

--- app/database/cloud2.sql
+++ app/database/cloud2.sql
@@ -1,4 +1,4 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.2.11
 -- http://www.phpmyadmin.net
 --
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `cloud`
 --
-
+CREATE DATABASE IF NOT EXIST 'cloud'
 
 -- --------------------------------------------------------
 
