d49622053d9fae24c376028cb81ce35e77280cce
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-11-15 11:09:01 +0100

    Commit 9.0

--- tests/SeleniumTest.php
+++ tests/SeleniumTest.php
@@ -1,25 +1,28 @@
 <?php
 class SeleniumTest extends \AjaxUnitTest {
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass ();
-		self::get ( "Selenium/index" );
+	public static function setUpBeforeClass(){
+		parent::setUpBeforeClass();
+		self::get("Selenium/index");
 	}
-	public function testDefault() {
-		$this->assertPageContainsText ( 'Hello Selenium' );
-		$this->assertTrue ( $this->elementExists ( "#text" ) );
-		$this->assertTrue ( $this->elementExists ( "#frm" ) );
+	
+	public function testDefault(){
+		$this->assertPageContainsText("Hello Selenium");
+		$this->assertTrue($this->elementExists("#frm"));
+		$this->assertTrue($this->elementExists("#text"));
 	}
-	public function testpost() {
-		$this->getElementById ( "text" )->sendKeys ( "okay" );
-		$this->getElementById ( "text" )->sendKeys ( "\xEE\x80\x87" );
-		SeleniumTest::$webDriver->manage ()->timeouts ()->implicitlyWait ( 5 );
-		$this->assertEquals ( "okay", $this->getElementById ( "result" )->getText () );
+	
+	public function testPost(){
+		$this->getElementById("text")->sendKeys("okay");
+		$this->getElementById("text")->sendKeys("\xEE\x80\x87");
+		SeleniumTest::$webDriver->manage()->timeouts()->implicitlyWait(5);
+		$this->assertEquals("okay",$this->getElementById("result")->getText());
 	}
-	public function testclick(){
-		self::get ( "Selenium/index" );
-		$this->getElementById ( "text2" )->sendKeys ( "testclick" );
-		$this->getElementById ( "btSubmit" )->click();
-		SeleniumTest::$webDriver->manage ()->timeouts ()->implicitlyWait ( 5 );
-		$this->assertEquals ( "testclick", $this->getElementById ( "result" )->getText () );
+	
+	public function testClick(){
+		self::get("Selenium/index");
+		$this->getElementById("text2")->sendKeys("test click");
+		$this->getElementById("btSubmit")->click();
+		SeleniumTest::$webDriver->manage()->timeouts()->implicitlyWait(5);
+		$this->assertEquals("test click",$this->getElementById("result")->getText());
 	}
 }
