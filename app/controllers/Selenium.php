743543735ead4120e8f05591d4c1de556d96c75c
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-11-15 11:06:54 +0100

    Commit 8.0

--- app/controllers/Selenium.php
+++ app/controllers/Selenium.php
@@ -1,5 +1,4 @@
 <?php
-
 use micro\utils\RequestUtils;
 class Selenium extends BaseController {
  
@@ -8,20 +7,18 @@ class Selenium extends BaseController {
     }
  
     public function index() {
-        //$this->initialize();
         echo "<h1>Hello Selenium</h1>";
         echo "<form method='POST' action='Selenium/post' name='frm' id='frm'>";
         echo "<input type='text' name='text' id='text'></form>";
         
         echo "<form method='POST' action='Selenium/post' name='frm2' id='frm2'>";
         echo "<input type='text' name='text' id='text2'>";
-        echo "<input type='submit' id='btSubmit' name='valide' value='Validez'></form>";
+        echo "<input type='submit' id='btSubmit' value='valider'></form>";
     }
  
     public function post(){
         if(RequestUtils::isPost()){
             echo "<div id='result'>".$_POST['text']."</div>";
-           
         }
     }
 }
